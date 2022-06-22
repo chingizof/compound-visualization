f9014347e602937bab8878b04052f6469c72507c
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-06-22 18:41:50 +0600

    divided app.js into a different components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import { Header } from './components/header';
+import { Container } from "./components/container"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className='container'>
-        <div className='container-description'>
-          <h1 className='container-text'>If you start investing ___$ every month, in ___ years you can buy...</h1>
-        </div>
-      </div>
+      <Container />
     </div>
   );
 }
--- frontend/src/components/container/index.jsx
+++ frontend/src/components/container/index.jsx
@@ -1,1 +1,11 @@
 import './index.css'
+
+export const Container =  () => {
+    return (
+        <div className='container'>
+            <div className='container-description'>
+                <h1 className='container-text'>If you start investing ___$ every month, in ___ years you can buy...</h1>
+            </div>
+        </div>
+    )
+}
